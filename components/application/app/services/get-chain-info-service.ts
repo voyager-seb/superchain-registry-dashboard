3ac84f4263079ffb0318bb722032a1de06a5d01d
voyager-seb <seb@kolektivo.co>
2024-08-19 14:58:40 -0500

    chore(api): fixed some typos and choring

--- components/application/app/services/get-chain-info-service.ts
+++ components/application/app/services/get-chain-info-service.ts
@@ -1,14 +1,16 @@
 import axios from "axios";
 import * as toml from "toml";
-import { ChainInfo } from "../types/chain-info"; 
+import { ChainInfo } from "../types/chain-info";
+
+const url =
+  "https://raw.githubusercontent.com/ethereum-optimism/superchain-registry/main/chainList.toml";
+const detail_url =
+  "https://raw.githubusercontent.com/ethereum-optimism/superchain-registry/main/superchain/configs/";
+
+const toml_extension = ".toml";
 
 export async function getChainInfoListService(): Promise<ChainInfo[]> {
   try {
-    const url =
-      "https://raw.githubusercontent.com/ethereum-optimism/superchain-registry/main/chainList.toml";
-    const detail_url =
-      "https://raw.githubusercontent.com/ethereum-optimism/superchain-registry/main/superchain/configs/";
-
     const chainInforesponse = await axios.get(url);
     const chainInfoparsedData = toml.parse(chainInforesponse.data);
 
@@ -23,15 +25,13 @@ export async function getChainInfoListService(): Promise<ChainInfo[]> {
 
           chainInfo.name = currentChain.name;
           chainInfo.layer = currentChain.parent.type;
-          chainInfo.status = currentChain.identifier.startsWith("mainnet")
-            ? "Mainnet"
-            : "Testnet";
-          chainInfo.configuration =
-            currentChain.superchain_level == 1 ? "Standard" : "Frontier";
-   
+          chainInfo.status = getStatus(currentChain.identifier);
+          chainInfo.configuration = getConfiguration(
+            currentChain.superchain_level
+          );
 
           await setChainInfoDetail(
-            detail_url + currentChain.identifier + ".toml",
+            detail_url + currentChain.identifier + toml_extension,
             chainInfo
           );
           chainInfoResult[index] = chainInfo;
@@ -56,19 +56,9 @@ async function setChainInfoDetail(
   const chainDetailresponse = await axios.get(url);
   const chainDetailToml = toml.parse(chainDetailresponse.data);
 
-  chainInfo.upgradeKeys =
-    chainDetailToml.addresses.ProxyAdminOwner.toUpperCase() ===
-    "0x5a0Aae59D09fccBdDb6C6CcEB07B7279367C3d2A".toUpperCase()
-      ? "Security Council"
-      : "Internal";
-  chainInfo.faultProofs = chainDetailToml.addresses.FaultDisputeGame
-    ? "Implemented"
-    : "Not implemented";
-  chainInfo.decentStage =
-    chainInfo.upgradeKeys == "Security Council" &&
-    chainInfo.faultProofs == "Implemented"
-      ? "Satege 1"
-      : "Stage 0";
+  chainInfo.upgradeKeys = getUpgradeKeys(chainDetailToml.addresses);
+  chainInfo.faultProofs = getFaultProofs(chainDetailToml.addresses);
+  chainInfo.decentStage = getDecentStage(chainInfo);
   chainInfo.charter = "N/A";
   chainInfo.charterLink = "";
   chainInfo.dataAvail = chainDetailToml.data_availability_type.toUpperCase();
@@ -76,4 +66,29 @@ async function setChainInfoDetail(
   chainInfo.blockTime = "~" + chainDetailToml.block_time;
 
   return chainInfo;
+}
+
+function getDecentStage(chainInfo: ChainInfo) {
+  return chainInfo.upgradeKeys == "Security Council" &&
+    chainInfo.faultProofs == "Implemented"
+    ? "Stage 1"
+    : "Stage 0";
+}
+
+function getFaultProofs(addresses: any) {
+  return addresses.FaultDisputeGame ? "Implemented" : "Not implemented";
+}
+function getUpgradeKeys(addresses: any) {
+  return addresses.ProxyAdminOwner.toUpperCase() ===
+    "0x5a0Aae59D09fccBdDb6C6CcEB07B7279367C3d2A".toUpperCase()
+    ? "Security Council"
+    : "Unspecified";
+}
+
+function getStatus(identifier: string) {
+  return identifier.startsWith("mainnet") ? "Mainnet" : "Testnet";
+}
+
+function getConfiguration(superchain_level: number) {
+  return superchain_level == 1 ? "Standard" : "Frontier";
 }
